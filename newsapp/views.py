244935e056ceb11cc10240f98de9d8d01e4547c5
girish <girishkamble181@gmail.com>
2021-02-25 22:22:51 +0530

    Adding newsapp

---
+++ newsapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+
+def home(request):
+	if request.method=="POST":
+		src = request.POST.get('src')
+		try:
+			a1 = "https://newsapi.org/v2/top-headlines"
+			a2 = "?sources=" + src
+			a3 = "&apiKey=" + "dcf42dec7d614e778d3dcd8616ab8182"  
+			wa = a1 + a2 + a3
+			res = requests.get(wa)
+			data = res.json()
+
+			info = data['articles']
+			return render(request,'home.html',{'info':info,'src':src})
+
+		except Exception as e:
+			return render(request,'home.html',{'err':'issue'})
+	else:
+		return render(request,'home.html')		
